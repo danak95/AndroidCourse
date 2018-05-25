cf5eb541627665fa2f5e75432e42800b31629ea9
KarinWas <karinw2512@gmail.com>
2018-05-25 11:41:14 +0300

    Finish designing adding new member window, without button for adding picture

--- app/src/main/java/com/example/kardana/androidcourse/ESC_Entrance.java
+++ app/src/main/java/com/example/kardana/androidcourse/ESC_Entrance.java
@@ -1,7 +1,10 @@
 package com.example.kardana.androidcourse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.PopupWindow;
 
 public class ESC_Entrance extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class ESC_Entrance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_esc__entrance);
+    }
+
+    // This function opens PopUp window of Login activity
+    public void clickLogin (View view){
+        Intent login_intent = new Intent(this, LoginActivity.class);
+        PopupWindow window = new PopupWindow(view, 400, 400, true);
+    }
+
+    public void clickSignIn (View view) {
+        Intent signin_intent = new Intent(this, AddNewMember.class);
+        startActivity(signin_intent);
     }
 }
