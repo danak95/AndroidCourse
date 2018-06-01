f8d61ac24d1210fa7ddd27226f16a19730202645
KarinWas <karinw2512@gmail.com>
2018-06-01 19:34:57 +0300

    Add new member- Added button for user image - by camera only

--- app/src/main/java/com/example/kardana/androidcourse/AddNewMember.java
+++ app/src/main/java/com/example/kardana/androidcourse/AddNewMember.java
@@ -1,13 +1,53 @@
 package com.example.kardana.androidcourse;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static android.app.Activity.RESULT_OK;
+
 
 public class AddNewMember extends AppCompatActivity {
 
-    @Override
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView avatar;
+    Bitmap imageBitmap;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_member);
+
+        Button editImage = this.findViewById(R.id.image_btn);
+        editImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open camera
+                Intent takePictureIntent = new Intent(
+                        MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+        avatar = this.findViewById(R.id.new_member_image);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE &&
+                resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            avatar.setImageBitmap(imageBitmap);
+        }
     }
 }
