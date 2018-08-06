aced5b3a96dd552c0885c40f990e9fa6a9bbc134
danak <danak1995@gmail.com>
2018-08-06 20:42:19 +0300

    application.getContext

--- app/src/main/java/com/example/kardana/androidcourse/RoomType.java
+++ app/src/main/java/com/example/kardana/androidcourse/RoomType.java
@@ -5,14 +5,14 @@ package com.example.kardana.androidcourse;
  */
 
 public enum RoomType {
-    HORROR(MainActivity.context.getString(R.string.horror)),
-    VR(MainActivity.context.getString(R.string.vr)),
-    ROLE_PLAYING(MainActivity.context.getString(R.string.role_playing)),
-    PERFORMANCE(MainActivity.context.getString(R.string.performance)),
-    COMEDY(MainActivity.context.getString(R.string.comedy)),
-    CULINARY(MainActivity.context.getString(R.string.culinary)),
-    WITH_ACTOR(MainActivity.context.getString(R.string.with_actor)),
-    WITHOUT_ACTOR(MainActivity.context.getString(R.string.without_actor));
+    HORROR(ESCApplication.getContext().getString(R.string.horror)),
+    VR(ESCApplication.getContext().getString(R.string.vr)),
+    ROLE_PLAYING(ESCApplication.getContext().getString(R.string.role_playing)),
+    PERFORMANCE(ESCApplication.getContext().getString(R.string.performance)),
+    COMEDY(ESCApplication.getContext().getString(R.string.comedy)),
+    CULINARY(ESCApplication.getContext().getString(R.string.culinary)),
+    WITH_ACTOR(ESCApplication.getContext().getString(R.string.with_actor)),
+    WITHOUT_ACTOR(ESCApplication.getContext().getString(R.string.without_actor));
 
     private String name;
 
